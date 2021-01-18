d8ad9d27c072e771782acfbca3c1bb5bcb8dc60c
Marcusvinimf <marcus_v_ina@hotmail.com>
2021-01-18 12:24:38 -0300

    desafio feito por pag publica

---
+++ desafioInsta.js
@@ -0,0 +1,16 @@
+let array1 = [1, 2, 3]
+let array2 = [12, 35, 9, 56, 24]
+
+function feito(x) {
+    
+    let novoArray = [x[x.length-1]];
+    
+    for(let = i=1; i<(x.length-1); i++){
+        novoArray.push(x[i])
+    }
+    novoArray.push(x[0])
+    return novoArray;
+}
+
+console.log(feito(array1));
+console.log(feito(array2));
